677c52ac97567fef2514a1ddd8b021f1b675b5cd
Ismail <mailismailpe@gmail.com>
2021-09-01 12:44:19 +0530

    added layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { Button, Layout, Menu } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
 import './App.css';
 
+const { Header, Content, Footer, Sider } = Layout;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={broken => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              Report 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              Report 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              Report 3
+            </Menu.Item>
+            <Menu.Item key="4" icon={<UserOutlined />}>
+              Report 4
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header className="site-layout-sub-header-background whiteText" style={{ padding: 0 }}>Report generator</Header>
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>ismail</Footer>
+        </Layout>
+      </Layout>
     </div>
   );
 }
